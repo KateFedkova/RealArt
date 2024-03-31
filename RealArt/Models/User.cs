0741b8e76e9ea5df6f8b4ed094b1c55d9e191b96
KateFedkova <kate.fedkova@gmail.com>
2024-03-31 20:28:13 +0300

    Create User model

---
+++ RealArt/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealArt.Models
+{
+    internal class User
+    {
+        public readonly Guid Id;
+
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string? Country { get; set; }
+        
+        public string? About { get; set; }
+        
+        public string? Foto { get; set; }
+
+        public List<Guid> Pictures { get; set; }
+        
+        public User(string username, string password)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Password = password;
+            Pictures = new List<Guid>();
+        }
+    }
+
+    
+}
