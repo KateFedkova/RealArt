c9afc03754e9b0725f5b0f39bc0df77cd6016a04
KateFedkova <kate.fedkova@gmail.com>
2024-03-31 21:14:33 +0300

    Create Auction model

---
+++ RealArt/Models/Auction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealArt.Models
+{
+    internal class Auction
+    {
+        public readonly Guid ID;
+
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public List<Painting> Pictures { get; set; }
+
+        public Auction(string name, string date, string time)
+        {
+            ID = Guid.NewGuid();
+            Name = name;
+            Date = date;
+            Time = time;
+            Pictures = new List<Painting>();
+        }
+    }
+}
