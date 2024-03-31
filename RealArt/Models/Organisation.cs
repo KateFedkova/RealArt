6b316dca5f9dd08599619855b646b0a4768b7e79
KateFedkova <kate.fedkova@gmail.com>
2024-03-31 20:59:30 +0300

    Create Organisation model

---
+++ RealArt/Models/Organisation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealArt.Models
+{
+    internal class Organisation : User
+    {
+        public string? Address { get; set; }
+        
+        public string? OpeningTime { get; set; }
+
+        public string? ClosingTime { get; set; }
+
+        public Organisation(string username, string password) : base(username, password)
+        {
+
+        }
+    }
+}
