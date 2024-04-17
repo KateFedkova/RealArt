f7f43f69c645cecc09d0a5563daf9b20bfdd6e87
KateFedkova <kate.fedkova@gmail.com>
2024-04-17 13:47:56 +0300

    Add switching screens

---
+++ RealArt/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealArt
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ToSignup_Click(object sender, EventArgs e)
+        {
+            SignUpForm signup = new SignUpForm(); 
+            signup.Show();
+            this.Hide();
+        }
+
+        private void ToMain_Click(object sender, EventArgs e)
+        {
+            MainForm main = new MainForm();
+            main.Show();
+            this.Hide();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
