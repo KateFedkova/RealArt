91c47bb6e4ba2ea1826da262c357e736ca30bb60
KateFedkova <kate.fedkova@gmail.com>
2024-04-23 23:21:45 +0300

    Add authentication logic

--- RealArt/LoginForm.cs
+++ RealArt/LoginForm.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using RealArt.Models;
+using System.Configuration;
+using System.Text.Json;
 
 namespace RealArt
 {
@@ -31,9 +25,109 @@ namespace RealArt
             this.Close();
         }
 
+        private void ToMainAfterLogin()
+        {
+            MainForm main = new MainForm();
+            main.Show();
+            this.Close();
+        }
+
         private void LoginButton_Click(object sender, EventArgs e)
         {
-        
+            string username = NameArea.Text;
+            string password = PasswordArea.Text;
+            string[] roles = { "Artists", "Сollectors", "Museums", "Organisations" };
+
+            if (CheckInfoIsGiven(username, password))
+            {
+                foreach (string role in roles)
+                {
+                    string? userJson = CheckUserRegistered(username, password, role);
+                    if (userJson != null)
+                    {
+
+                        if (role == "Artists" || role == "Сollectors")
+                        {
+                            CurrentUser<Person>.Info = JsonSerializer.Deserialize<Person?>(userJson);
+                            CurrentUser<Person>.Role = role == "Artists" ? "Artist" : "Collector";
+                            ToMainAfterLogin();
+                            return;
+                        }
+                        else if (role == "Museums" || role == "Organisations")
+                        {
+                            CurrentUser<Organisation>.Info = JsonSerializer.Deserialize<Organisation?>(userJson);
+                            CurrentUser<Organisation>.Role = role == "Museums" ? "Museum" : "Organisation";
+                            ToMainAfterLogin();
+                            return;
+                        }
+                     }
+                }
+
+                MessageBox.Show("Такий користувач не зареєстрований");
+                return;
+            }
+
+            else
+            {
+                MessageBox.Show("Не всі дані були надані");
+            }
+        }
+
+        private bool CheckInfoIsGiven(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private string? CheckUserRegistered(string username, string password, string role)
+        {
+            string[] jsonLines = ReadFile(role);
+
+            foreach (string jsonLine in jsonLines)
+            {
+
+                if (role == "Artists" || role == "Сollectors")
+                {
+                    Person? user = JsonSerializer.Deserialize<Person?>(jsonLine);
+
+                    if (user?.Username == username && VerifyPassword(password, user.Password))
+                    {
+                        return jsonLine;
+                    }
+                }
+
+                else if (role == "Museums" || role == "Organisations")
+                {
+                    Organisation? user = JsonSerializer.Deserialize<Organisation?>(jsonLine);
+
+                    if (user?.Username == username && VerifyPassword(password, user.Password))
+                    {
+                        return jsonLine;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private string[] ReadFile(string role)
+        { 
+            string? data = ConfigurationManager.AppSettings["PathTo" + role + "Data"];
+
+            string jsonData = File.ReadAllText(data);
+
+            string[] jsonLines = jsonData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            return jsonLines;
+        }
+
+        private bool VerifyPassword(string password, string passwordHash)
+        {
+            bool result = BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash);
+            return result;
         }
     }
 }
