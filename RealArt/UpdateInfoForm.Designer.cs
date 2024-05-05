5edde741e30f574b66b5da2c63a93dcc743a66e2
KateFedkova <kate.fedkova@gmail.com>
2024-05-05 22:13:33 +0300

    Create UpdateInfo design

---
+++ RealArt/UpdateInfoForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace RealArt
+{
+    partial class UpdateInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            roundButton2 = new AdditionalElements.RoundButton();
+            roundButton1 = new AdditionalElements.RoundButton();
+            ClosingTimePicker = new DateTimePicker();
+            AddressTextbox = new TextBox();
+            StyleTextbox = new TextBox();
+            AboutTextbox = new TextBox();
+            label7 = new Label();
+            OpeningTimePicker = new DateTimePicker();
+            CoutryChooser = new ComboBox();
+            ClosingLabel = new Label();
+            OpeningLabel = new Label();
+            AddressLabel = new Label();
+            StyleLabel = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(roundButton2);
+            panel1.Controls.Add(roundButton1);
+            panel1.Controls.Add(ClosingTimePicker);
+            panel1.Controls.Add(AddressTextbox);
+            panel1.Controls.Add(StyleTextbox);
+            panel1.Controls.Add(AboutTextbox);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(OpeningTimePicker);
+            panel1.Controls.Add(CoutryChooser);
+            panel1.Controls.Add(ClosingLabel);
+            panel1.Controls.Add(OpeningLabel);
+            panel1.Controls.Add(AddressLabel);
+            panel1.Controls.Add(StyleLabel);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(-10, -14);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(849, 707);
+            panel1.TabIndex = 0;
+            // 
+            // roundButton2
+            // 
+            roundButton2.BackColor = Color.MediumSlateBlue;
+            roundButton2.BackgroundColor = Color.MediumSlateBlue;
+            roundButton2.BorderColor = Color.Red;
+            roundButton2.BorderRadius = 20;
+            roundButton2.BorderSize = 0;
+            roundButton2.FlatAppearance.BorderSize = 0;
+            roundButton2.FlatStyle = FlatStyle.Flat;
+            roundButton2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            roundButton2.ForeColor = Color.White;
+            roundButton2.Location = new Point(597, 599);
+            roundButton2.Name = "roundButton2";
+            roundButton2.Size = new Size(180, 55);
+            roundButton2.TabIndex = 15;
+            roundButton2.Text = "Не змінювати";
+            roundButton2.TextColor = Color.White;
+            roundButton2.UseVisualStyleBackColor = false;
+            // 
+            // roundButton1
+            // 
+            roundButton1.BackColor = Color.MediumSlateBlue;
+            roundButton1.BackgroundColor = Color.MediumSlateBlue;
+            roundButton1.BorderColor = Color.Red;
+            roundButton1.BorderRadius = 20;
+            roundButton1.BorderSize = 0;
+            roundButton1.FlatAppearance.BorderSize = 0;
+            roundButton1.FlatStyle = FlatStyle.Flat;
+            roundButton1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            roundButton1.ForeColor = Color.White;
+            roundButton1.Location = new Point(455, 599);
+            roundButton1.Name = "roundButton1";
+            roundButton1.Size = new Size(105, 55);
+            roundButton1.TabIndex = 14;
+            roundButton1.Text = "OK";
+            roundButton1.TextColor = Color.White;
+            roundButton1.UseVisualStyleBackColor = false;
+            // 
+            // ClosingTimePicker
+            // 
+            ClosingTimePicker.Format = DateTimePickerFormat.Time;
+            ClosingTimePicker.Location = new Point(52, 566);
+            ClosingTimePicker.Name = "ClosingTimePicker";
+            ClosingTimePicker.ShowUpDown = true;
+            ClosingTimePicker.Size = new Size(300, 31);
+            ClosingTimePicker.TabIndex = 9;
+            // 
+            // AddressTextbox
+            // 
+            AddressTextbox.Location = new Point(455, 384);
+            AddressTextbox.MaxLength = 120;
+            AddressTextbox.Multiline = true;
+            AddressTextbox.Name = "AddressTextbox";
+            AddressTextbox.Size = new Size(322, 117);
+            AddressTextbox.TabIndex = 13;
+            // 
+            // StyleTextbox
+            // 
+            StyleTextbox.Location = new Point(52, 301);
+            StyleTextbox.MaxLength = 120;
+            StyleTextbox.Multiline = true;
+            StyleTextbox.Name = "StyleTextbox";
+            StyleTextbox.Size = new Size(322, 58);
+            StyleTextbox.TabIndex = 5;
+            // 
+            // AboutTextbox
+            // 
+            AboutTextbox.Location = new Point(455, 170);
+            AboutTextbox.MaxLength = 120;
+            AboutTextbox.Multiline = true;
+            AboutTextbox.Name = "AboutTextbox";
+            AboutTextbox.Size = new Size(322, 117);
+            AboutTextbox.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 11F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.Location = new Point(287, 45);
+            label7.Name = "label7";
+            label7.Size = new Size(273, 30);
+            label7.TabIndex = 1;
+            label7.Text = "Update your personal info";
+            // 
+            // OpeningTimePicker
+            // 
+            OpeningTimePicker.Format = DateTimePickerFormat.Time;
+            OpeningTimePicker.Location = new Point(52, 444);
+            OpeningTimePicker.Name = "OpeningTimePicker";
+            OpeningTimePicker.ShowUpDown = true;
+            OpeningTimePicker.Size = new Size(300, 31);
+            OpeningTimePicker.TabIndex = 7;
+            // 
+            // CoutryChooser
+            // 
+            CoutryChooser.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            CoutryChooser.FormattingEnabled = true;
+            CoutryChooser.Items.AddRange(new object[] { "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "The Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Republic of the Costa Rica", "Côte d’Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "The Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Federated States of Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Sudan, South", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe" });
+            CoutryChooser.Location = new Point(52, 170);
+            CoutryChooser.Name = "CoutryChooser";
+            CoutryChooser.Size = new Size(322, 36);
+            CoutryChooser.TabIndex = 3;
+            // 
+            // ClosingLabel
+            // 
+            ClosingLabel.AutoSize = true;
+            ClosingLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ClosingLabel.Location = new Point(52, 510);
+            ClosingLabel.Name = "ClosingLabel";
+            ClosingLabel.Size = new Size(133, 30);
+            ClosingLabel.TabIndex = 8;
+            ClosingLabel.Text = "Closing time";
+            // 
+            // OpeningLabel
+            // 
+            OpeningLabel.AutoSize = true;
+            OpeningLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            OpeningLabel.Location = new Point(52, 394);
+            OpeningLabel.Name = "OpeningLabel";
+            OpeningLabel.Size = new Size(146, 30);
+            OpeningLabel.TabIndex = 6;
+            OpeningLabel.Text = "Opening time";
+            // 
+            // AddressLabel
+            // 
+            AddressLabel.AutoSize = true;
+            AddressLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            AddressLabel.Location = new Point(455, 329);
+            AddressLabel.Name = "AddressLabel";
+            AddressLabel.Size = new Size(91, 30);
+            AddressLabel.TabIndex = 12;
+            AddressLabel.Text = "Address";
+            // 
+            // StyleLabel
+            // 
+            StyleLabel.AutoSize = true;
+            StyleLabel.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            StyleLabel.Location = new Point(52, 245);
+            StyleLabel.Name = "StyleLabel";
+            StyleLabel.Size = new Size(59, 30);
+            StyleLabel.TabIndex = 4;
+            StyleLabel.Text = "Style";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(455, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 30);
+            label2.TabIndex = 10;
+            label2.Text = "About";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(52, 116);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 30);
+            label1.TabIndex = 2;
+            label1.Text = "Country";
+            // 
+            // UpdateInfoForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(828, 683);
+            ControlBox = false;
+            Controls.Add(panel1);
+            MaximumSize = new Size(850, 802);
+            MinimumSize = new Size(850, 705);
+            Name = "UpdateInfoForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label StyleLabel;
+        private Label label2;
+        private Label AddressLabel;
+        private Label OpeningLabel;
+        private Label ClosingLabel;
+        private ComboBox CoutryChooser;
+        private DateTimePicker OpeningTimePicker;
+        private TextBox AboutTextbox;
+        private Label label7;
+        private TextBox AddressTextbox;
+        private TextBox StyleTextbox;
+        private DateTimePicker ClosingTimePicker;
+        private AdditionalElements.RoundButton roundButton1;
+        private AdditionalElements.RoundButton roundButton2;
+    }
+}
