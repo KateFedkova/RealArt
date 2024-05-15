146e895b18c661208ffd8bf173774f8fda4658ac
KateFedkova <kate.fedkova@gmail.com>
2024-05-15 23:26:45 +0300

    Add changing UI in AddPainting based on the role

---
+++ RealArt/Forms/AddPaintingForm.cs
@@ -0,0 +1,42 @@
+﻿using RealArt.Models;
+using System.Configuration;
+using System.Text.Json;
+
+namespace RealArt
+{
+    public partial class AddPaintingForm : Form
+    {
+        public AddPaintingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPaintingForm_Load(object sender, EventArgs e)
+        {
+            string? imageNotFound = ConfigurationManager.AppSettings["ImageNotFound"];
+
+            if (imageNotFound != null)
+            {
+                PaintingPictureBox.Image = Image.FromFile(imageNotFound); 
+            }
+
+            if (CurrentUser.Info is Person)
+            {
+                PersonElementsVisibility();
+            }
+        }
+
+        private void PersonElementsVisibility()
+        {
+            ArtistLabel.Visible = false;
+            ArtistTextBox.Visible = false;
+            TypeLabel.Visible = false;
+            OriginalRadio.Visible = false;
+            CopyRadio.Visible = false;
+            PriceLabel.Visible = false;
+            PriceTextBox.Visible = false;
+            OkButton.Location = new Point(825, 216);
+            CancelButton.Location = new Point(820, 300);
+        }
+    }
+}
