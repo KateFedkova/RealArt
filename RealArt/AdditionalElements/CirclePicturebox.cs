cc5749ae552d1ec8feb961340b8eea8ce9d9642d
KateFedkova <kate.fedkova@gmail.com>
2024-05-18 13:41:29 +0300

    Add CirclePicturebox control

---
+++ RealArt/AdditionalElements/CirclePicturebox.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace RealArt.AdditionalElements
+{
+    internal class CirclePicturebox : PictureBox
+    {
+
+        public CirclePicturebox()
+        {
+            this.BackColor = Color.DarkGray;
+        }
+        
+        protected override void OnResize(EventArgs e)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+            graphicsPath.AddEllipse(0, 0, this.Width, this.Height);
+            Region region = new Region(graphicsPath);
+            this.Region = region;
+        }
+    }
+}
